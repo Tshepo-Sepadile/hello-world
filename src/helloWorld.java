4ca9c9cc1a793a224a298e0d4e98c69a298e9308
Tshepo-Sepadile <tsheposepadile1995@gmail.com>
2019-02-16 16:13:10 +0200

    First commit

---
+++ src/helloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tshepo S
+ */
+import javax.swing.JOptionPane;
+
+public class helloWorld {
+    
+    public static void main(String[]args)
+    {
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+    
+}
